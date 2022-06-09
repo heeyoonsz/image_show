f19e6800734caa231cdbee888f2a843392a0e82b
heeyoonsz <gmldbs0812@duksung.ac.kr>
2022-06-09 23:22:31 +0900

    0609

--- app/src/main/java/ds/project/tadaktadakfront/SetImageActivity.kt
+++ app/src/main/java/ds/project/tadaktadakfront/SetImageActivity.kt
@@ -1,12 +1,15 @@
 ﻿package ds.project.tadaktadakfront
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import java.net.URI
+
 
 
 class SetImageActivity : AppCompatActivity() {
@@ -22,5 +25,15 @@ class SetImageActivity : AppCompatActivity() {
             .error(ColorDrawable(Color.RED))
             .placeholder(R.drawable.ic_launcher_foreground)
             .into(imageView)
+
+        // 2초 후 다음 액티비티로 넘김
+        Handler(Looper.getMainLooper()).postDelayed({
+
+
+
+
+        }, 2000)
+
+
     }
 }
